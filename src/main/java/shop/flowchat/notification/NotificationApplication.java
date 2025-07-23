f392b666e8044824207a38f9f55a312b6f6c2ee1
Chanui <jcu011@naver.com>
2025-07-23 22:25:20 +0900

    fix: @EnableJpaAuditing 추가

--- src/main/java/shop/flowchat/notification/NotificationApplication.java
+++ src/main/java/shop/flowchat/notification/NotificationApplication.java
@@ -2,7 +2,9 @@ package shop.flowchat.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NotificationApplication {
 
