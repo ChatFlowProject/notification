dd2cef2734a9a3d6fb46ef988cfc0f92651d14fd
Dayun Kim <ozsally0201@gmail.com>
2025-07-26 02:32:59 +0900

    chore: 서버 기본 시간대를 Asia/Seoul로 설정

--- src/main/java/shop/flowchat/notification/NotificationApplication.java
+++ src/main/java/shop/flowchat/notification/NotificationApplication.java
@@ -1,5 +1,7 @@
 package shop.flowchat.notification;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +12,12 @@ public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
