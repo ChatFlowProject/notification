787560c91bde981e5903c9669340b5e4757b0fc8
Dayun Kim <ozsally0201@gmail.com>
2025-07-27 22:19:06 +0900

    feat: CORS 설정 추가

---
+++ src/main/java/shop/flowchat/notification/infrastructure/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package shop.flowchat.notification.infrastructure.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    private static final List<String> ALLOWED_ORIGIN = List.of("http://localhost:3000",
+            "http://flowchat.shop:30100", "https://flowchat.shop:30200",
+            "https://chatflow-project.netlify.app");
+    private static final List<String> ALLOWED_ORIGIN_PATTERN = List.of("http://*.flowchat.shop",
+            "https://*.flowchat.shop");
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALLOWED_ORIGIN.toArray(new String[0]))
+                .allowedOriginPatterns(ALLOWED_ORIGIN_PATTERN.toArray(new String[0]))
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("X-Requested-With", "Origin", "Accept", "Content-Type", "Authorization", "X-XSRF-token")
+                .allowCredentials(true)
+                .maxAge(3600); // preflight 요청 결과 캐시 시간
+    }
+}
