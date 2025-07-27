639fc4b4e218ba15f056cb30c72a09ca2fd12a31
Dayun Kim <ozsally0201@gmail.com>
2025-07-28 05:19:16 +0900

    refactor: team, channel 로직 제거

--- src/main/java/shop/flowchat/notification/event/consumer/ChannelEventConsumer.java
+++ src/main/java/shop/flowchat/notification/event/consumer/ChannelEventConsumer.java
@@ -5,18 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import shop.flowchat.notification.external.dto.channel.ChannelUpdatedEvent;
-import shop.flowchat.notification.external.dto.team.TeamUpdatedEvent;
-import shop.flowchat.notification.query.NotificationChannelQuery;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChannelEventConsumer {
-    private final NotificationChannelQuery channelQuery;
 
-    @KafkaListener(topics = "channel", groupId = "notification-group")
+    @KafkaListener(topics = "channel")
     public void consume(ChannelUpdatedEvent event) {
-        log.info("ChannelUpdatedEvent : {}", event);
-        channelQuery.update(event);
+
     }
 }
--- src/main/java/shop/flowchat/notification/event/consumer/TeamEventConsumer.java
+++ src/main/java/shop/flowchat/notification/event/consumer/TeamEventConsumer.java
@@ -5,17 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import shop.flowchat.notification.external.dto.team.TeamUpdatedEvent;
-import shop.flowchat.notification.query.NotificationTeamQuery;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TeamEventConsumer {
-    private final NotificationTeamQuery teamQuery;
 
-    @KafkaListener(topics = "team", groupId = "notification-group")
+    @KafkaListener(topics = "team")
     public void consume(TeamUpdatedEvent event) {
-        log.info("TeamUpdatedEvent : {}", event);
-        teamQuery.update(event);
+
     }
 }
